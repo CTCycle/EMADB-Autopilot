f60aa3d196a5d290b427d224ef56760d418af643
CTCycle <thomas.virdis89@gmail.com>
2023-10-31 10:55:48 +0100

    added exception handling

--- EMADB_launcher.py
+++ EMADB_launcher.py
@@ -1,7 +1,6 @@
 import os
 import re
 import art
-import pandas as pd
 from collections import defaultdict
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
@@ -82,10 +81,13 @@ for letter, drugs in grouped_drugs.items():
     letter_xpath = f'//a[@onclick="showProductTable(\'{letter.lower()}\')"]'
     webscraper.autoclick(20, letter_xpath)
     for d in drugs:
-        placeholder = webscraper.drug_finder(30, d) 
-        excel_ph = webscraper.excel_downloader(30, GlobVar.download_path) 
-        DAP_path = os.path.join(GlobVar.download_path, 'DAP.xlsx')
-        rename_path = os.path.join(GlobVar.download_path, f'{d}.xlsx')
-        os.rename(DAP_path, rename_path) 
+        try:
+            placeholder = webscraper.drug_finder(30, d) 
+            excel_ph = webscraper.excel_downloader(30, GlobVar.download_path) 
+            DAP_path = os.path.join(GlobVar.download_path, 'DAP.xlsx')
+            rename_path = os.path.join(GlobVar.download_path, f'{d}.xlsx')
+            os.rename(DAP_path, rename_path) 
+        except:
+            print(f'An error has been encountered while fetching {d} data. Skipping this drug...')
 
 
