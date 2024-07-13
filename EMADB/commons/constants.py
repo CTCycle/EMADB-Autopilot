9502b67ca6ff2f42f74ec575458253c46fa798a8
CTCycle <thomas.virdis89@gmail.com>
2024-07-13 18:25:03 +0200

    changed path in constants

--- EMADB/commons/constants.py
+++ EMADB/commons/constants.py
@@ -4,7 +4,7 @@ from os.path import join, dirname, abspath
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 DATA_PATH = join(PROJECT_DIR, 'resources')
 DOWNLOAD_PATH = join(DATA_PATH, 'download')
-LOGS_PATH = join(PROJECT_DIR, 'resources', 'logs')
+LOGS_PATH = join(DATA_PATH, 'logs')
 
 CONFIG_PATH = join(PROJECT_DIR, 'settings', 'configurations.json')
 with open(CONFIG_PATH, 'r') as file:
