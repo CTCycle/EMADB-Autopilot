9c62c73529dee01f7f311a9e7fe82b58519d964e
CTCycle <thomas.virdis89@gmail.com>
2025-07-09 00:03:53 +0200

    changed logger format

--- EMADB/commons/logger.py
+++ EMADB/commons/logger.py
@@ -22,7 +22,7 @@ LOG_CONFIG =  {
             'datefmt': '%d-%m-%Y %H:%M:%S'
         },        
         'minimal': {
-            'format': '%(message)s',
+            'format': '[%(levelname)s] %(message)s',
         },
     },    
     
